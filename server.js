c5000b179ba100ef64e69a164dc59b1104122797
samjw <wagnerjsamuel@gmail.com>
2019-03-26 17:34:05 -0400

    saving not working, attmpt fix

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 const mongoose = require("mongoose");
-const mongoURL = process.env.PROD_MONGODB || "mongodb://localhost:27017/googlebooks"
+const mongoURL = process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks"
 mongoose.connect(mongoURL, {useNewUrlParser: true})
   .then(() => {
     console.log("🗄 ==> Successfully connected to mongoDB.");
